32c6189c5599358a0781a140d798f7cd5dafa9de
Ben Hamlin <protob3n@gmail.com>
2014-04-01 14:19:12 -0700

    Fix handling of incorrect address/hostname

--- src/geoip.c
+++ src/geoip.c
@@ -53,8 +53,8 @@ void ipdata_dbfree(db_t *dbp)
 
 ipdata_t *ipdata_lookup(const char *inaddr, db_t *dbp)
 {
-  char *city, *region, *country, *asnum;
-  double latitude, longitude;
+  char *city, *region, *country, *asnum, *empty = "X";
+  city = region = country = asnum = empty;
   ipdata_t *ip = NULL;
   GeoIPRecord *grp = NULL;
 
@@ -64,17 +64,17 @@ ipdata_t *ipdata_lookup(const char *inaddr, db_t *dbp)
   if(dbp->citygp)
     if((grp = GeoIP_record_by_name(dbp->citygp, inaddr)))
     {
-      city = (grp && grp->city) ? grp->city : "X";
-      region = (grp && grp->region) ? grp->region : "X";
-      country = (grp && grp->country_code) ? grp->country_code : "X";
-      latitude = (grp && grp->latitude) ? grp->latitude : 0;
-      longitude = (grp && grp->longitude) ? grp->longitude : 0;
+      if(grp->city) city = grp->city;
+      if(grp->region) region = grp->region;
+      if(grp->country_code) country = grp->country_code;
+      ip->latitude = grp->latitude;
+      ip->longitude = grp->longitude;
     }
 
   /* Query the asnum database, if we have it. */
   if(dbp->asgp)
     if(!(asnum = GeoIP_org_by_name(dbp->asgp, inaddr)))
-      asnum = "X";
+      asnum = empty;
 
   ip = MALLOC(sizeof(ipdata_t), "ipdata_lookup()");
 
@@ -87,12 +87,10 @@ ipdata_t *ipdata_lookup(const char *inaddr, db_t *dbp)
   strcpy(ip->region, region);
   ip->country = MALLOC(strlen(country) + 1, "ipdata_lookup()");
   strcpy(ip->country, country);
-  ip->latitude = latitude;
-  ip->longitude = longitude;
   ip->asnum = MALLOC(strlen(asnum) + 1, "ipdata_lookup()");
   strcpy(ip->asnum, asnum);
 
-  GeoIPRecord_delete(grp);
+  if(grp) GeoIPRecord_delete(grp);
   return ip;
 }
 
